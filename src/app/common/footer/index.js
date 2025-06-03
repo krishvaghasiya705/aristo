a9d1bf58b2b03f077fa41e64e39a96334c388da8
krish vaghasiya <kvaghasiya705@gmail.com>
2025-06-03 18:42:10 +0530

    feat: Add footer component with styles and logo integration
    - Implemented a new Footer component with structured layout and styles.
    - Added logo with link functionality to the footer.
    - Introduced SCSS styles for footer layout and design enhancements.

--- src/app/common/footer/index.js
+++ src/app/common/footer/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import styles from "./footer.module.scss"
+import Footerlogo from '@/assets/icon/footerlogo'
+import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div>
-      
-    </div>
+    <footer className={styles.footermain}>
+      <div className="container">
+        <div className={styles.footer}>
+          <div className={styles.footerlogo}>
+            <Link href={"/"}>
+              <Footerlogo />
+            </Link>
+          </div>
+          
+          <div className={styles.footercontentmain}></div>
+        </div>
+      </div>
+    </footer>
   )
 }
